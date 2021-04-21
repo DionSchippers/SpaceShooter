310b20be768b7f716daeb1a58443cf57754a8d21
Dion <dionschippers@gmail.com>
2021-04-21 10:53:04 +0200

    moviement

--- core/src/com/spaceshooter/game/SpaceShooter.java
+++ core/src/com/spaceshooter/game/SpaceShooter.java
@@ -1,31 +1,108 @@
 package com.spaceshooter.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
-public class SpaceShooter extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
+public class SpaceShooter extends ApplicationAdapter implements InputProcessor {
+    SpriteBatch batch;
+    Texture img;
+    Sprite sprite;
+    boolean movingRight = false;
+    boolean movingLeft = false;
 
-	@Override
-	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        img = new Texture("spaceship.png");
+        sprite = new Sprite(img);
+        sprite.setPosition(Gdx.graphics.getWidth() / 2 - sprite.getWidth() / 2, 20);
+        Gdx.input.setInputProcessor(this);
+    }
+
+    @Override
+    public void render() {
+        if (movingRight) {
+            if (sprite.getX() > Gdx.graphics.getWidth()-96) {
+                sprite.setX(Gdx.graphics.getWidth()-96);
+            }else {
+                sprite.translateX(15f);
+            }
+        }
+        if (movingLeft) {
+            if (sprite.getX() < 0) {
+                sprite.setX(0);
+            }else {
+                sprite.translateX(-15f);
+            }
+        }
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        batch.begin();
+        batch.draw(sprite, sprite.getX(), sprite.getY(),96,96);
+        batch.end();
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        img.dispose();
+    }
+
+    @Override
+    public boolean keyDown(int keycode) {
+        if (keycode == Input.Keys.A)
+            movingLeft = true;
+
+        if (keycode == Input.Keys.D)
+            movingRight = true;
+        return false;
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        if (keycode == Input.Keys.A) {
+            movingLeft = false;
+        }
+        if (keycode == Input.Keys.D) {
+            movingRight = false;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean keyTyped(char character) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        return false;
+    }
+
+    @Override
+    public boolean mouseMoved(int screenX, int screenY) {
+        return false;
+    }
+
+    @Override
+    public boolean scrolled(float amountX, float amountY) {
+        return false;
+    }
 }
