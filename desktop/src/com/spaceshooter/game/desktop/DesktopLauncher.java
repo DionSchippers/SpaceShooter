713f3eef86d03ee2012b6bf10371d189edc94ddf
Storm Heg <storm@stormheg.co>
2021-04-26 10:40:17 +0200

    Update to lwjgl3

--- desktop/src/com/spaceshooter/game/desktop/DesktopLauncher.java
+++ desktop/src/com/spaceshooter/game/desktop/DesktopLauncher.java
@@ -1,14 +1,14 @@
 package com.spaceshooter.game.desktop;
 
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.spaceshooter.game.SpaceShooter;
 
 public class DesktopLauncher {
+
 	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.height = 1080;
-		config.width = 1920;
-		new LwjglApplication(new SpaceShooter(), config);
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setWindowSizeLimits(800, 600, 800, 600);
+		new Lwjgl3Application(new SpaceShooter(), config);
 	}
 }
